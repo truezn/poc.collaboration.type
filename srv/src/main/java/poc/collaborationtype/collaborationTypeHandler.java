e973a362ab5226aa8ac9f144ff01ed2999b8976e
Ning Zhang <ni.zhang@sap.com>
2019-07-19 09:22:20 +0800

    add logic for set the state default value

--- srv/src/main/java/poc/collaborationtype/collaborationTypeHandler.java
+++ srv/src/main/java/poc/collaborationtype/collaborationTypeHandler.java
@@ -6,7 +6,10 @@ import java.util.Map;
 
 import com.sap.cloud.sdk.service.prov.api.annotations.Action;
 import com.sap.cloud.sdk.service.prov.api.exception.DatasourceException;
+import com.sap.cloud.sdk.service.prov.api.operations.Create;
+import com.sap.cloud.sdk.service.prov.api.request.CreateRequest;
 import com.sap.cloud.sdk.service.prov.api.request.OperationRequest;
+import com.sap.cloud.sdk.service.prov.api.response.CreateResponse;
 import com.sap.cloud.sdk.service.prov.api.response.ErrorResponse;
 import com.sap.cloud.sdk.service.prov.api.response.OperationResponse;
 import com.sap.cloud.sdk.service.prov.api.DataSourceHandler;
@@ -14,6 +17,38 @@ import com.sap.cloud.sdk.service.prov.api.EntityData;
 import com.sap.cloud.sdk.service.prov.api.ExtensionHelper;
 
 public class collaborationTypeHandler {
+	
+	@Create(serviceName = "CollaborationType", entity = "CollaborationTypeMetadata")
+	public CreateResponse addCustomer(CreateRequest createRequest, ExtensionHelper extensionHelper) {
+
+//	    logger.info("Creating Customer");
+	    DataSourceHandler handler = extensionHelper.getHandler();
+
+	    EntityData entityData = createRequest.getData();
+	    Integer state = 1; 
+//	    set default 
+	    entityData = EntityData.getBuilder(entityData).removeElement("state").addElement("state", state).buildEntityData("CollaborationTypeMetadata");
+	    /* The executeInsert method takes two parameters:
+	     *  1. entityData containing the current request data
+	     *  2. a boolean value that indicates whether the created entity needs to be returned. If true,
+	     *    the handler would internally make another GET call and return the created entity as entityData.
+	     *    Here we give this value as true since we need to return it back in the CreateResponse.
+	     */
+	    try {
+	    	
+	    	EntityData createdEntity = handler.executeInsert(entityData, true);
+	    	CreateResponse createResponse = CreateResponse.setSuccess().setData(createdEntity).response();
+		    return createResponse;
+
+	    } catch (DatasourceException e) {
+			// Return an instance of OperationResponse containing the error in
+			// case of failure
+			ErrorResponse errorResponse = ErrorResponse.getBuilder().setMessage(e.getMessage()).setCause(e).response();
+			return CreateResponse.setError(errorResponse);
+		}
+	    
+
+	}
 
 	@Action(Name = "setToUse", serviceName = "CollaborationType")
 	public OperationResponse setToUse(OperationRequest actionRequest, ExtensionHelper extensionHelper) {
