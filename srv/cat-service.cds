db3c873389dde204b8341883d7dc1e2e5575f41f
Ning Zhang <ni.zhang@sap.com>
2019-07-29 07:28:30 +0000

    add external API

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,7 +1,10 @@
 using ning.CollaborationType as my from '../db/CollaborationTypeMetadata';
 using ning.CollaborationTypePurpose as purpose from '../db/CollaborationTypePurpose';
+using API_ENTERPRISE_PROJECT_SRV as enterproject from   './external/csn/EnterpriseProjectCreateReadUpdateDelete';
 service CollaborationType {
     entity CollaborationTypeMetadata as projection on my.CollaborationTypeMetadata;
     action setToUse(collaborationTypeId:String, state:Integer, version:String);
     entity CollaborationTypePurpose as projection on purpose.CollaborationTypePurpose;
+    entity EnterpriseProjects       as projection on enterproject.A_EnterpriseProjectType
+                                       excluding {to_EnterpriseProjectElement, to_EnterpriseProjectJVA};
 }
--- srv/src/main/java/poc/collaborationtype/CollaborationType.java
+++ srv/src/main/java/poc/collaborationtype/CollaborationType.java
@@ -1,6 +1,5 @@
 package poc.collaborationtype;
 
-
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
@@ -20,7 +19,7 @@ import com.sap.cloud.sdk.service.prov.api.response.CreateResponse;
 import com.sap.cloud.sdk.service.prov.api.response.ErrorResponse;
 import com.sap.cloud.sdk.service.prov.api.response.OperationResponse;
 import com.sap.cloud.sdk.service.prov.api.response.QueryResponse;
-import com.sap.it.commons.logging.slf4j.LoggerFactory;
+
 import com.sap.cloud.sdk.odatav2.connectivity.ODataException;
 import com.sap.cloud.sdk.s4hana.connectivity.ErpConfigContext;
 import com.sap.cloud.sdk.service.prov.api.DataSourceHandler;
@@ -31,36 +30,37 @@ import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultEnterpriseProjec
 import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.enterpriseproject.EnterpriseProject;
 
 public class CollaborationType {
-	
+
 	@Create(serviceName = "CollaborationType", entity = "CollaborationTypeMetadata")
 	public CreateResponse addCollaborationTypeMetadata(CreateRequest createRequest, ExtensionHelper extensionHelper) {
 
-//	    logger.info("Creating Customer");
-	    DataSourceHandler handler = extensionHelper.getHandler();
+		// logger.info("Creating Customer");
+		DataSourceHandler handler = extensionHelper.getHandler();
 
-	    EntityData entityData = createRequest.getData();
-	    Integer state = 1; 
-//	    set default 
-	    entityData = EntityData.getBuilder(entityData).removeElement("state").addElement("state", state).buildEntityData("CollaborationTypeMetadata");
-	    /* The executeInsert method takes two parameters:
-	     *  1. entityData containing the current request data
-	     *  2. a boolean value that indicates whether the created entity needs to be returned. If true,
-	     *    the handler would internally make another GET call and return the created entity as entityData.
-	     *    Here we give this value as true since we need to return it back in the CreateResponse.
-	     */
-	    try {
-	    	
-	    	EntityData createdEntity = handler.executeInsert(entityData, true);
-	    	CreateResponse createResponse = CreateResponse.setSuccess().setData(createdEntity).response();
-		    return createResponse;
+		EntityData entityData = createRequest.getData();
+		Integer state = 1;
+		// set default
+		entityData = EntityData.getBuilder(entityData).removeElement("state").addElement("state", state)
+				.buildEntityData("CollaborationTypeMetadata");
+		/*
+		 * The executeInsert method takes two parameters: 1. entityData containing the
+		 * current request data 2. a boolean value that indicates whether the created
+		 * entity needs to be returned. If true, the handler would internally make
+		 * another GET call and return the created entity as entityData. Here we give
+		 * this value as true since we need to return it back in the CreateResponse.
+		 */
+		try {
 
-	    } catch (DatasourceException e) {
+			EntityData createdEntity = handler.executeInsert(entityData, true);
+			CreateResponse createResponse = CreateResponse.setSuccess().setData(createdEntity).response();
+			return createResponse;
+
+		} catch (DatasourceException e) {
 			// Return an instance of OperationResponse containing the error in
 			// case of failure
 			ErrorResponse errorResponse = ErrorResponse.getBuilder().setMessage(e.getMessage()).setCause(e).response();
 			return CreateResponse.setError(errorResponse);
 		}
-	    
 
 	}
 
@@ -74,15 +74,18 @@ public class CollaborationType {
 		keys.put("version", String.valueOf(parameters.get("version")));
 		// fetching the collaborationType details for the is and version
 		try {
-			EntityData entityData = handler.executeRead("CollaborationTypeMetadata", keys, Arrays.asList("collaborationTypeId", "version"));
-//			EntityData entityData = new DefaultEntityData();
-		Integer state = 3; 
-//	    update the state value 
-	    entityData = EntityData.getBuilder(entityData).removeElement("state").addElement("state", state).buildEntityData("CollaborationTypeMetadata");
-//	    updating the CollaborationTypeMetadata
-	    handler.executeUpdate(entityData, keys, false);
-	    
-			OperationResponse response = OperationResponse.setSuccess().setEntityData(Arrays.asList(entityData)).response();
+			EntityData entityData = handler.executeRead("CollaborationTypeMetadata", keys,
+					Arrays.asList("collaborationTypeId", "version"));
+			// EntityData entityData = new DefaultEntityData();
+			Integer state = 3;
+			// update the state value
+			entityData = EntityData.getBuilder(entityData).removeElement("state").addElement("state", state)
+					.buildEntityData("CollaborationTypeMetadata");
+			// updating the CollaborationTypeMetadata
+			handler.executeUpdate(entityData, keys, false);
+
+			OperationResponse response = OperationResponse.setSuccess().setEntityData(Arrays.asList(entityData))
+					.response();
 
 			return response;
 		} catch (DatasourceException e) {
@@ -94,44 +97,42 @@ public class CollaborationType {
 		}
 
 	}
-	
-//	Logger logger = LoggerFactory.getLogger(S4BookshopService.class);
 
-	private static final String DESTINATION_NAME = "APIHubProject"; //Refers to the destination created in Step 6
-	private static final String apikey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; //Replace with your API key
+	// Logger logger = LoggerFactory.getLogger(S4BookshopService.class);
+
+	private static final String DESTINATION_NAME = "APIHubProject"; // Refers to the destination created in Step 6
+	private static final String apikey = "dJ6uFzfpbYFvTB3AUz2H1wL4AURielDX"; // Replace with your API key
 
 	private ErpConfigContext context = new ErpConfigContext(DESTINATION_NAME);
 
-	@Query(serviceName = "CollaborationType", entity = "CollaborationTypeMetadata")
+	@Query(serviceName = "CollaborationType", entity = "EnterpriseProjects")
 	public QueryResponse queryCollaborationTypeMetadata(QueryRequest queryRequest) {
 		QueryResponse queryResponse;
-	    int top = queryRequest.getTopOptionValue();
-	    int skip = queryRequest.getSkipOptionValue();
+		int top = queryRequest.getTopOptionValue();
+		int skip = queryRequest.getSkipOptionValue();
 
-	    try {
-	    	// Create Map containing request header information
-	        	Map<String, String> requestHeaders = new HashMap<>();
-	        	requestHeaders.put("Content-Type","application/json");
-	        	requestHeaders.put("APIKey",apikey);
+		try {
+			// Create Map containing request header information
+			Map<String, String> requestHeaders = new HashMap<>();
+			requestHeaders.put("Content-Type", "application/json");
+			requestHeaders.put("APIKey", apikey);
 
-	    	final List<EnterpriseProject> enterpriseProject =
-	    	             new DefaultEnterpriseProjectService().getAllEnterpriseProject()
-	    	            .withCustomHttpHeaders(requestHeaders).onRequestAndImplicitRequests()
-	    	            .select(EnterpriseProject.ALL_FIELDS)
-	    	            .top(top >= 0 ? top : 50)
-	    	            .skip(skip >= 0 ? skip : -1)
-	    	           .execute(context);
-	    	queryResponse = QueryResponse.setSuccess().setData(enterpriseProject).response();
+			final List<EnterpriseProject> enterpriseProject = new DefaultEnterpriseProjectService()
+					.getAllEnterpriseProject().withCustomHttpHeaders(requestHeaders).onRequestAndImplicitRequests()
+					.select(EnterpriseProject.ALL_FIELDS).top(top >= 0 ? top : 50).skip(skip >= 0 ? skip : -1)
+					.execute(context);
+			queryResponse = QueryResponse.setSuccess().setData(enterpriseProject).response();
 
-	    } catch (final ODataException e) {
-//	    	logger.error("Error occurred with the Query operation: " + e.getMessage(), e);
-	    	ErrorResponse er = ErrorResponse.getBuilder()
-	    	                            .setMessage("Error occurred with the Query operation: " + e.getMessage())
-	    	                            .setStatusCode(500).setCause(e).response();
-	    	queryResponse = QueryResponse.setError(er);
-	    }
+		} catch (final ODataException e) {
+			// logger.error("Error occurred with the Query operation: " + e.getMessage(),
+			// e);
+			ErrorResponse er = ErrorResponse.getBuilder()
+					.setMessage("Error occurred with the Query operation: " + e.getMessage()).setStatusCode(500)
+					.setCause(e).response();
+			queryResponse = QueryResponse.setError(er);
+		}
 
-		  return queryResponse;
+		return queryResponse;
 	}
 
 }
