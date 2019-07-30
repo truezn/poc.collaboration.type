c09e886a6fcbfd96aa0439a417c103f9b9d25934
Ning Zhang <ni.zhang@sap.com>
2019-07-30 05:25:45 +0000

    add more api call of BP from external

--- srv/cat-service.cds
+++ srv/cat-service.cds
@@ -1,7 +1,10 @@
 using poc.CollaborationType as db from '../db/data-model';
-
+using API_BUSINESS_PARTNER as bp from '../srv/external/csn/ODataServiceforBusinessPartner';
 service CollaborationType {
     entity CollaborationTypeMetadata as projection on db.CollaborationTypeMetadata;
     action setToUse(collaborationTypeId:String, state:Integer, version:String);
     entity CollaborationTypePurpose as projection on db.CollaborationTypePurpose;
+    entity Suppliers as projection on bp.A_SupplierType
+	   excluding {to_SupplierCompany, to_SupplierPurchasingOrg};
+    
 }
--- srv/src/main/java/poc/collaborationtype/CollaborationType.java
+++ srv/src/main/java/poc/collaborationtype/CollaborationType.java
@@ -26,10 +26,24 @@ import com.sap.cloud.sdk.service.prov.api.DataSourceHandler;
 import com.sap.cloud.sdk.service.prov.api.EntityData;
 import com.sap.cloud.sdk.service.prov.api.ExtensionHelper;
 
+
+// import com.sap.cloud.sdk.s4hana.connectivity.*;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.businesspartner.Supplier;
+import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultBusinessPartnerService;
+
 // import com.sap.cloud.sdk.s4hana.datamodel.odata.services.DefaultEnterpriseProjectService;
 // import com.sap.cloud.sdk.s4hana.datamodel.odata.namespaces.enterpriseproject.EnterpriseProject;
 
 public class CollaborationType {
+	
+	
+	
+	// Logger logger = LoggerFactory.getLogger(S4BookshopService.class);
+
+	private static final String DESTINATION_NAME = "APIHubProject"; // Refers to
+	private static final String apikey = "dJ6uFzfpbYFvTB3AUz2H1wL4AURielDX"; 
+	private ErpConfigContext context = new ErpConfigContext(DESTINATION_NAME);
+
 
 	@Create(serviceName = "CollaborationType", entity = "CollaborationTypeMetadata")
 	public CreateResponse addCollaborationTypeMetadata(CreateRequest createRequest, ExtensionHelper extensionHelper) {
@@ -96,6 +110,39 @@ public class CollaborationType {
 			return OperationResponse.setError(errorResponse);
 		}
 
+	}
+	
+	@Query(serviceName = "CollaborationType", entity = "Suppliers")
+	public QueryResponse querySupplier(QueryRequest qryRequest) {
+
+    QueryResponse queryResponse;
+    int top = qryRequest.getTopOptionValue();
+    int skip = qryRequest.getSkipOptionValue();
+
+    try {
+    	// Create Map containing request header information
+        	Map<String, String> requestHeaders = new HashMap<>();
+        	requestHeaders.put("Content-Type","application/json");
+        	requestHeaders.put("APIKey",apikey);
+
+    	final List<Supplier> suppliers =
+    	             new DefaultBusinessPartnerService().getAllSupplier()
+    	            .withCustomHttpHeaders(requestHeaders).onRequestAndImplicitRequests()
+    	            .select(Supplier.SUPPLIER, Supplier.SUPPLIER_NAME)
+    	            .top(top >= 0 ? top : 50)
+    	            .skip(skip >= 0 ? skip : -1)
+    	           .execute(context);
+    	queryResponse = QueryResponse.setSuccess().setData(suppliers).response();
+
+    } catch (final ODataException e) {
+    	logger.error("Error occurred with the Query operation: " + e.getMessage(), e);
+    	ErrorResponse er = ErrorResponse.getBuilder()
+    	                            .setMessage("Error occurred with the Query operation: " + e.getMessage())
+    	                            .setStatusCode(500).setCause(e).response();
+    	queryResponse = QueryResponse.setError(er);
+    }
+
+	  return queryResponse;
 	}
 
 	// Logger logger = LoggerFactory.getLogger(S4BookshopService.class);
